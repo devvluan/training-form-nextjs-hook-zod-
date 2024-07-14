5e41304f0f9432b12ac96ae5f1c7154f1770bbbf
Luan Sellera <luansellera98@gmail.com>
2024-07-14 04:22:38 -0300

    adicionando pasta

---
+++ src/lib/supabase.ts
@@ -0,0 +1,5 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient('https://gagzzyjqayftbxmwmlnl.supabase.co',
+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdhZ3p6eWpxYXlmdGJ4bXdtbG5sIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyMDk0MDQ3MywiZXhwIjoyMDM2NTE2NDczfQ.jR-llh-s_sa-SL5m-y3gmrtGsxWMiB9RuRtyaTKgR1A'
+)
